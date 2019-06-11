58308f7b4fcd101edb57b5b36d6347115707c595
Ariel Quintanilla <ariel.q64623@gmail.com>
2019-06-11 12:12:25 -0500

    animation + movement

--- GameModel/Assets/Scripts/enemyAI.cs
+++ GameModel/Assets/Scripts/enemyAI.cs
@@ -13,6 +13,7 @@ public class enemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         
     }
 
--- GameModel/Assets/Scripts/playerControls.cs
+++ GameModel/Assets/Scripts/playerControls.cs
@@ -7,23 +7,29 @@ public class playerControls : MonoBehaviour
     Rigidbody2D rb;
     Vector2 jump;
 
-    public float moveSpeed = 4.0f;
-    public float jumpForce = 8.0f;
     public float attackTimer = 0;
     public float attackCooldown = 0.3f;
-    public bool isGrounded = true;
 
     public LayerMask groundLayer;
     public Transform groundedEnd;
+    public Transform crouchCeilingEnd;
+    public Animator animator;
 
     public Collider2D attackTriggerLeft;
     public Collider2D attackTriggerRight;
     public Collider2D attackTriggerUp;
     public Collider2D attackTriggerDown;
 
+    public Collider2D standCollider;
+
     float distDown;
 
+    bool isGrounded = true;
     bool attack = false;
+    bool crouch = false;
+
+    float moveSpeed;
+    float jumpForce;
 
     Vector3 pos;
 
@@ -31,13 +37,17 @@ public class playerControls : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        jump = new Vector2(0.0f, jumpForce);
         distDown = GetComponent<Collider2D>().bounds.extents.y;
 
         attackTriggerLeft.enabled = false;
         attackTriggerRight.enabled = false;
         attackTriggerUp.enabled = false;
         attackTriggerDown.enabled = false;
+
+        standCollider.enabled = true;
+
+        moveSpeed = 8.0f;
+        jumpForce = 4.0f;
     }
 
     // Update is called once per frame
@@ -45,7 +55,9 @@ public class playerControls : MonoBehaviour
     {
         pos = transform.position; //position of the player
         isGrounded = Physics2D.Linecast(pos, groundedEnd.position, 1 << LayerMask.NameToLayer("Ground")); //line from middle to bottom of player
-        //the line detects the Ground layer objects
+        jump = new Vector2(0.0f, jumpForce);
+        animator.SetFloat("Speed", Mathf.Abs(Input.GetAxisRaw("Horizontal")*moveSpeed));
+        animator.SetBool("Ground", isGrounded);
 
         //Player Controls
         if (Input.GetKey(KeyCode.A)) // move left
@@ -56,14 +68,19 @@ public class playerControls : MonoBehaviour
         {
             pos.x += moveSpeed * Time.deltaTime;
         }
-        if (Input.GetKeyDown(KeyCode.W) && isGrounded) //jump when grounded
+        if (Input.GetKey(KeyCode.W) && isGrounded) //jump when grounded
         {
             rb.AddForce(jump, ForceMode2D.Impulse);
+            animator.SetTrigger("Jump");
             isGrounded = false;
         }
         if(Input.GetKey(KeyCode.S) && !isGrounded)
         {
             rb.AddForce(new Vector2(0.0f, -0.5f), ForceMode2D.Impulse);
+        }
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            crouch = true;
         }
 
         if(Input.GetKey(KeyCode.RightArrow) && !attack)
@@ -105,6 +122,30 @@ public class playerControls : MonoBehaviour
                 attackTriggerUp.enabled = false;
                 attackTriggerDown.enabled = false;
             }
+        }
+
+        if (crouch)
+        {
+            if (!Input.GetKey(KeyCode.LeftControl))
+            {
+                if (Physics2D.Linecast(pos, crouchCeilingEnd.position, 1 << LayerMask.NameToLayer("Ground")))
+                {
+                    //can't stand up here
+                }
+                else
+                {
+                    crouch = false;
+                }
+            }
+            standCollider.enabled = false;
+            moveSpeed = 2.0f;
+            jumpForce = 4.0f;
+        }
+        else
+        {
+            standCollider.enabled = true;
+            moveSpeed = 4.0f;
+            jumpForce = 8.0f;
         }
 
         transform.position = pos; // update position
