f68ed6d4b11868dd5d7c0d0cc8c02377211dbe48
Sujay Sharma <sjsujay96@gmail.com>
2019-07-22 00:22:20 -0500

    Main Menu

---
+++ GameModel/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void QuitGame()
+    {
+        // No visible changes in the unity editor hence the debug.log to make sure it works 
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
