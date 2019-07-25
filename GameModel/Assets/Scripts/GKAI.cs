5a813cf2968be0ee6331da7a2d9f99c411f20634
Luis Flores <luisflores21095@yahoo.com>
2019-07-24 20:20:38 -0500

    boss damage animation added (level 1)

--- GameModel/Assets/Scripts/GKAI.cs
+++ GameModel/Assets/Scripts/GKAI.cs
@@ -9,11 +9,13 @@ public class GKAI : MonoBehaviour
     public int cEnum = 0;
 
     public Transform edgeCheck;
+    public Transform player;
     public Transform wallCheck;
     public Transform playerCheck;
     public Collider2D attackTriggerFront;
     public Animator animator;
 
+    bool hurt = false;
     bool isGrounded = true;
     bool wallAhead = false;
     bool playerAhead = false;
@@ -48,77 +50,80 @@ public class GKAI : MonoBehaviour
         playerAhead = Physics2D.Linecast(pos, playerCheck.position, 1 << LayerMask.NameToLayer("Player")); //sets true if player is ahead
         animator.SetBool("isAttacking", attack);
 
-
-        if (charging) {
-
-            if ( cEnum ==6)
-            {
-
-                attackTriggerFront.enabled = true;
-                cEnum = 0;
-            }
-            else {
-                attackTriggerFront.enabled = false;
-
-                
-                cEnum += 1;
-            }
-
-        }
-
-        if (!playerAhead && !turning && !throwing && !attack)
+        if (!hurt)
         {
-
-
-
-            if (isGrounded && !wallAhead) //if there is ground ahead, and no wall ahead, keep moving
+            if (charging)
             {
-                pos.x += moveSpeed * Time.deltaTime;
+
+                if (cEnum == 6)
+                {
+
+                    attackTriggerFront.enabled = true;
+                    cEnum = 0;
+                }
+                else
+                {
+                    attackTriggerFront.enabled = false;
+
+
+                    cEnum += 1;
+                }
+
             }
-            else  //else, turn around
+
+            if (!playerAhead && !turning && !throwing && !attack)
             {
-                animator.SetBool("TurnAround", true);
-                turning = true;
+
+
+
+                if (isGrounded && !wallAhead) //if there is ground ahead, and no wall ahead, keep moving
+                {
+                    pos.x += moveSpeed * Time.deltaTime;
+                }
+                else  //else, turn around
+                {
+                    animator.SetBool("TurnAround", true);
+                    turning = true;
+                    oldMoveSpeed = moveSpeed; //save current moving direction
+                    moveSpeed = 0;
+                    // moveSpeed *= -1;
+                    //facingRight = !facingRight;
+                    // Vector2 charScale = transform.localScale;
+                    //charScale.x *= -1;
+                    //transform.localScale = charScale;
+                }
+            }
+
+
+
+            if (!attack && playerAhead && !turning && !charging && !throwing) //if player is ahead, attack
+            {
                 oldMoveSpeed = moveSpeed; //save current moving direction
-                moveSpeed = 0;
-                // moveSpeed *= -1;
-                //facingRight = !facingRight;
-                // Vector2 charScale = transform.localScale;
-                //charScale.x *= -1;
-                //transform.localScale = charScale;
+                moveSpeed = 0; //stop moving to start attack
+                animator.SetInteger("attackNum", Random.Range(0, 3));
+
+
+                attack = true;
+                attackTimer = attackCooldown;
             }
-        }
-
-
-
-        if (!attack && playerAhead && !turning && !charging && !throwing) //if player is ahead, attack
-        {
-            oldMoveSpeed = moveSpeed; //save current moving direction
-            moveSpeed = 0; //stop moving to start attack
-            animator.SetInteger("attackNum", Random.Range(0, 3));
-
-            
-            attack = true;
-            attackTimer = attackCooldown;
-        }
 
 
 
 
-        if (attack && !turning && !charging)
-        {
-            if (attackTimer < (attackCooldown - 0.3))
+            if (attack && !turning && !charging)
             {
-                attackTriggerFront.enabled = true;
+                if (attackTimer < (attackCooldown - 0.3))
+                {
+                    attackTriggerFront.enabled = true;
+                }
+                if (attackTimer > 0)
+                {
+                    attackTimer -= Time.deltaTime;
+                }
+
             }
-            if (attackTimer > 0)
-            {
-                attackTimer -= Time.deltaTime;
-            }
-          
+
         }
-
-
 
         transform.position = pos;
 
@@ -131,91 +136,69 @@ public class GKAI : MonoBehaviour
 
     public void Damage(int dmg) //detects attack from player (can add HP and stuff here later)
     {
-        Destroy(gameObject);
+        if (!hurt && !attack && !charging)
+        {
+            animator.SetTrigger("hurt");
+
+            hurt = true;
+            if (player.transform.position.x >= gameObject.transform.position.x)
+            {
+
+                if (facingRight)
+                {
+                    moveSpeed *= -1;
+                    facingRight = !facingRight;
+                    Vector2 charScale = transform.localScale;
+                    charScale.x *= -1;
+                    transform.localScale = charScale;
+                }
+
+            }
+            else
+            {
+
+                if (!facingRight)
+                {
+
+                    moveSpeed *= -1;
+                    facingRight = !facingRight;
+                    Vector2 charScale = transform.localScale;
+                    charScale.x *= -1;
+                    transform.localScale = charScale;
+                }
+
+
+            }
+        }
     }
-   
+
     public void AlertObservers(string message)
     {
-        
-        if (message.Equals("attack"))
+        if (message.Equals("screamEnd"))
         {
-            attack = false;
-            attackTriggerFront.enabled = false;
-            moveSpeed = oldMoveSpeed; //once attack has finished, start moving again
+            hurt = false;
+            charging = true;
+            attackTriggerFront.enabled = true;
+
+            animator.SetTrigger("isCharging");
             if (moveSpeed >= 0)
             {
-                moveSpeed = 1f;
+                moveSpeed = 6f;
             }
             else
             {
-                moveSpeed = -1f;
+                moveSpeed = -6f;
             }
+
+            charging = true;
 
         }
-
-        if (message.Equals("startThrowing"))
         {
-            ThrowRock(1);
-
-        }
-
-        if (message.Equals("throwing"))
-        {
-            throwing = false;
-            animator.SetTrigger("isWalking");
-            if (moveSpeed >= 0)
+            if (message.Equals("attack"))
             {
-                moveSpeed = 1f;
-            }
-            else
-            {
-                moveSpeed = -1f;
-            }
-
-        }
-
-
-
-
-        if (message.Equals("turning"))
-        {
-            animator.SetBool("TurnAround", false);
-
-            moveSpeed = oldMoveSpeed;
-            attackTriggerFront.enabled = false;
-
-            charging = false;
-            turning = false;
-            // Do other things based on an attack ending.
-
-            facingRight = !facingRight;
-            Vector2 charScale = transform.localScale;
-            charScale.x *= -1;
-            transform.localScale = charScale;
-            moveSpeed *= -1;
-            randomNext = Random.Range(0, 4);
-
-            if (randomNext == 0)
-            {
-                charging = true;
-                attackTriggerFront.enabled = true;
-
-                animator.SetTrigger("isCharging");
-                if (moveSpeed >= 0)
-                {
-                    moveSpeed = 6f;
-                }
-                else
-                {
-                    moveSpeed = -6f;
-                }
-
-                charging = true;
-            }
-            else if (randomNext == 1)
-            {
-                animator.SetTrigger("isWalking");
-
+                attack = false;
+                attackTriggerFront.enabled = false;
+                moveSpeed = oldMoveSpeed; //once attack has finished, start moving again
                 if (moveSpeed >= 0)
                 {
                     moveSpeed = 1f;
@@ -226,26 +209,101 @@ public class GKAI : MonoBehaviour
                 }
 
             }
-            else if (randomNext == 2)
+
+            if (message.Equals("startThrowing"))
             {
-                animator.SetTrigger("isThrowing");
-                throwing = true;
+                ThrowRock(1);
 
             }
-            else if (randomNext == 3)
-            {
-                animator.SetTrigger("isRunning");
 
+            if (message.Equals("throwing"))
+            {
+                throwing = false;
+                animator.SetTrigger("isWalking");
                 if (moveSpeed >= 0)
                 {
-                    moveSpeed = 6f;
+                    moveSpeed = 1f;
                 }
                 else
                 {
-                    moveSpeed = -6f;
+                    moveSpeed = -1f;
                 }
 
+            }
 
+
+
+
+            if (message.Equals("turning"))
+            {
+                animator.SetBool("TurnAround", false);
+
+                moveSpeed = oldMoveSpeed;
+                attackTriggerFront.enabled = false;
+
+                charging = false;
+                turning = false;
+                // Do other things based on an attack ending.
+
+                facingRight = !facingRight;
+                Vector2 charScale = transform.localScale;
+                charScale.x *= -1;
+                transform.localScale = charScale;
+                moveSpeed *= -1;
+                randomNext = Random.Range(0, 4);
+
+                if (randomNext == 0)
+                {
+                    charging = true;
+                    attackTriggerFront.enabled = true;
+
+                    animator.SetTrigger("isCharging");
+                    if (moveSpeed >= 0)
+                    {
+                        moveSpeed = 6f;
+                    }
+                    else
+                    {
+                        moveSpeed = -6f;
+                    }
+
+                    charging = true;
+                }
+                else if (randomNext == 1)
+                {
+                    animator.SetTrigger("isWalking");
+
+                    if (moveSpeed >= 0)
+                    {
+                        moveSpeed = 1f;
+                    }
+                    else
+                    {
+                        moveSpeed = -1f;
+                    }
+
+                }
+                else if (randomNext == 2)
+                {
+                    animator.SetTrigger("isThrowing");
+                    throwing = true;
+
+                }
+                else if (randomNext == 3)
+                {
+                    animator.SetTrigger("isRunning");
+
+                    if (moveSpeed >= 0)
+                    {
+                        moveSpeed = 6f;
+                    }
+                    else
+                    {
+                        moveSpeed = -6f;
+                    }
+
+
+                }
             }
         }
     }
